58f949b2721748b9aab08d4ff8eb679a2893cc49
Patrick Kahardipraja <patrick.khdipraja@gmail.com>
2019-05-16 14:34:09 +0200

    Fixed for docker

--- build.sh
+++ build.sh
@@ -7,13 +7,7 @@ fi
 
 cd build
 
-cmake -Dboost_filesystem_DIR=/usr/local/lib/cmake/boost_filesystem-1.70.0 \ 
-      -Dboost_headers_DIR= /usr/local/lib/cmake/boost_headers-1.70.0 \ 
-      -Dboost_python_DIR=/usr/local/lib/cmake/boost_python-1.70.0 \
-      -Dboost_system_DIR=/usr/local/lib/cmake/boost_system-1.70.0 \
-      -DCMAKE_INSTALL_PREFIX=/usr/local \ 
-      -DBUILD_SHARED_LIBS=ON \ 
-      -DBoost_USE_STATIC_LIBS=OFF ../
+cmake ../
 make
 touch __init__.py
 
@@ -24,4 +18,4 @@ rm -rf opencv_contrib
 cd ..
 
 wget http://farm1.static.flickr.com/121/278839518_140821637d.jpg -O sample.jpg
-python scripts/test_bing.py
+python3 scripts/test_bing.py
